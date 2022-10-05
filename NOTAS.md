1de8d0bc5e30b9243f7a914899064c2c56187ca5
unknown <juan.lozada1905@gmail.com>
2022-10-04 21:34:17 -0400

    App Pokeindex

---
+++ NOTAS.md
@@ -0,0 +1,4 @@
+# NOTAS
+
+1) Instalar react router dom como dependencia
+npm install react-router-dom@6
---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import home from './Home'
+import { BrowserRouter } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route></Route>
+    </Routes>
+    </BrowserRouter>
+      
+    </>
+  );
+}
+
+export default App;
